2a84486133fdbeb7802706363f35b211f3945554
Michal Ozogán <michal@ozogan.eu>
2019-10-20 15:19:05 +0200

    Changed: Use new release config

--- release.config.js
+++ release.config.js
@@ -1,26 +1,3 @@
-const path = require('path');
-const { prepare } = require('semantic-release-config-fabulator');
-
-const dockerPackage = `fabulator/${process.env.npm_package_name}`;
-
 module.exports = {
-    extends: 'semantic-release-config-fabulator',
-    verifyConditions: ['semantic-release-docker'],
-    prepare: [
-        //...prepare.filter((value) => value !== '@semantic-release/npm' && value !== '@semantic-release/git'),
-        {
-            path: '@semantic-release/exec',
-            cmd: `docker build -t ${dockerPackage} .`,
-        }
-    ],
-    publish: [
-        //'@semantic-release/github',
-        {
-            path: path.resolve(__dirname, './publish-docker.js'),
-            name: `${dockerPackage}`,
-        },
-    ],
-    success: [],
-    fail: [],
-    ci: false,
+    extends: 'semantic-release-config-fabulator/docker',
 };
