65693b87b93af52527287d011a3c895d794720fb
Michal Ozogán <michal@ozogan.eu>
2019-10-20 13:25:50 +0200

    Fixed: Missing packages

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 const { prepare } = require('semantic-release-config-fabulator');
 
-const dockerPackage = `fabulator/${process.env.npm_package_name}:${nextRelease.version}`;
+const dockerPackage = `fabulator/${process.env.npm_package_name}:\${nextRelease.version}`;
 
 module.exports = {
     extends: 'semantic-release-config-fabulator',
