7e62a658e54c1f9b83fbbc865286187740a699b6
Michal Ozogán <michal@ozogan.eu>
2019-10-20 13:24:24 +0200

    Added: Building docker image

--- release.config.js
+++ release.config.js
@@ -1,10 +1,25 @@
 const { prepare } = require('semantic-release-config-fabulator');
 
+const dockerPackage = `fabulator/${process.env.npm_package_name}:${nextRelease.version}`;
+
 module.exports = {
     extends: 'semantic-release-config-fabulator',
     verifyConditions: ['semantic-release-docker'],
-    prepare: prepare.filter((value) => value !== '@semantic-release/npm' && value !== '@semantic-release/git'),
-    publish: [/*'@semantic-release/github',*/ 'semantic-release-docker'],
+    prepare: [
+        ...prepare.filter((value) => value !== '@semantic-release/npm' && value !== '@semantic-release/git'),
+        {
+            path: '@semantic-release/exec',
+            cmd: `docker build -t ${dockerPackage} .`,
+        }
+    ],
+    publish: [
+        //'@semantic-release/github',
+        {
+            path: 'semantic-release-docker',
+            name: `${dockerPackage}`,
+        },
+    ],
     success: [],
     fail: [],
+    ci: false,
 };
