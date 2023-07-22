23b5f4a5007c2cade20001e25b8cd4c095ed9bea
Michal Ozogán <michal.ozogan@ceskatelevize.cz>
2023-07-22 20:58:10 +0200

    Fixed: Upgrade twitter client

--- src/tweet.ts
+++ src/tweet.ts
@@ -1,15 +1,20 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import Twitter from 'twitter';
 import { DateTime } from 'luxon';
+import { TwitterApi } from 'twitter-api-v2';
 
-const client = new Twitter({
-    consumer_key: process.env.TWITTER_KEY,
-    consumer_secret: process.env.TWITTER_SECRET,
-    access_token_key: process.env.TWITTER_TOKEN,
-    access_token_secret: process.env.TWITTER_SECRETTOKEN,
+export const client = new TwitterApi({
+    appKey: process.env.TWITTER_KEY,
+    appSecret: process.env.TWITTER_SECRET,
+    accessToken: process.env.TWITTER_TOKEN,
+    accessSecret: process.env.TWITTER_SECRETTOKEN,
 });
 
-export default (text: string) => {
+export default async (text: string) => {
     console.log(`${DateTime.local().toISO()} - ${text}`);
-    return client.post('statuses/update', { status: text });
+
+    try {
+        return await client.v2.tweet({ text });
+    } catch (error) {
+        console.error(error);
+    }
 };
